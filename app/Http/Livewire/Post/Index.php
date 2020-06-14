9b48373b8faed9d148b72d217ca7eee962a05a07
Riffadi Taufik Hidayah <riffadi.th@gmail.com>
2020-06-14 22:15:19 +0800

    show data in livewire

--- app/Http/Livewire/Post/Index.php
+++ app/Http/Livewire/Post/Index.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Livewire\Post;
 
+use App\Post;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Index extends Component
 {
+
+    use withPagination;
+    // public $posts;
+    
     public function render()
     {
-        return view('livewire.post.index');
+        return view('livewire.post.index', [
+            'posts' => Post::latest()->paginate(5)
+        ]);
+
+        # another way
+        // $this->posts = Post::latest()->paginate(5);
     }
 }
