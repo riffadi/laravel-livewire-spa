e08591290ff120d317e9acd4fe4198cf2d036879
Riffadi Taufik Hidayah <riffadi.th@gmail.com>
2020-06-14 23:28:52 +0800

    update data into database

--- app/Http/Livewire/Post/Edit.php
+++ app/Http/Livewire/Post/Edit.php
@@ -2,12 +2,62 @@
 
 namespace App\Http\Livewire\Post;
 
+use App\Post;
 use Livewire\Component;
 
 class Edit extends Component
 {
+    // Define Variable
+
+    public $title;
+    public $content;
+    public $postId;
+
+    // Mount or construct function 
+
+    public function mount($id)
+    {
+        $post = Post::find($id);
+
+        if($post) {
+            $this->title = $post->title;
+            $this->content = $post->content;
+            $this->postId = $post->id;
+        }
+    }
+
+    // Update Funtion
+
+    public function edit()
+    {
+        $this->validate([
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        if($this->postId)
+        {
+            $post = Post::find($this->postId);
+
+            if($post){
+                $post->update([
+                    'title' => $this->title,
+                    'content' => $this->content
+                ]);
+            }
+        }
+
+        //  Flash Message
+        session()->flash('message', 'Data edited successfully.');
+
+        return redirect()->route('post.index');
+    }
+
+    
     public function render()
     {
         return view('livewire.post.edit');
     }
+
+
 }
--- resources/views/livewire/post/edit.blade.php
+++ resources/views/livewire/post/edit.blade.php
@@ -1,3 +1,28 @@
 <div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    <div class="card">
+        <div class="card-body">
+            <input type="hidden" wire:model="postId">
+            <form wire:submit.prevent="edit">
+                <div class="form-group">
+                    <label>Title</label>
+                    <input type="text" wire:model="title" class="form-control @error('title') is-invalid @enderror" placeholder="Enter Title">
+                    @error('title')
+                        <span class="invalid-feedback">
+                            {{ $message }}
+                        </span>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <label>Content</label>
+                    <textarea type="text" wire:model="content" class="form-control @error('content') is-invalid @enderror" rows="4" placeholder="Enter Content"></textarea>
+                    @error('title')
+                        <span class="invalid-feedback">
+                            {{ $message }}
+                        </span>
+                    @enderror
+                </div>
+                <button type="submit" class="btn btn-primary">Save</button>
+            </form>
+        </div>
+    </div>
 </div>
